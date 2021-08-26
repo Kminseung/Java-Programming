487e0c4805472257c00aba859cc3fd81e1043388
Kminseung <soccer6504@gmail.com>
2021-08-26 16:43:51 +0900

    Loop - while

---
+++ Java-Programming/src/LoopConditional/Loop1.java
@@ -0,0 +1,15 @@
+package LoopConditional;
+
+public class Loop1 {
+
+	public static void main(String[] args) {
+
+		int i = 1, sum = 0;
+		while(i <= 1000) {
+			sum += i++;
+		}
+
+		System.out.println("1부터 1000까지 합은 " + sum + "입니다.");
+	}
+
+}
