45ca37bad14792e2e3c318fcfb17da7e1fe368bd
Kminseung <soccer6504@gmail.com>
2021-08-26 16:05:46 +0900

    Data Type - String, substring

---
+++ Java-Programming/src/DataType/SubString.java
@@ -0,0 +1,16 @@
+package DataType;
+
+public class SubString {
+
+	public static void main(String[] args) {
+
+		String name = "John Doe";
+		System.out.println(name);
+		System.out.println(name.substring(0, 1));
+		System.out.println(name.substring(3, 7));
+		System.out.println(name.substring(5, 8));
+		System.out.println(name.substring(0, 4));
+		
+	}
+
+}
