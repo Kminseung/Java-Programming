30541c771573cd9fa12b7a33c5deb4b51810231d
Kminseung <soccer6504@gmail.com>
2021-08-26 16:05:14 +0900

    Data Type - Notation

---
+++ Java-Programming/src/DataType/Notation.java
@@ -0,0 +1,15 @@
+package DataType;
+
+public class Notation {
+
+	public static void main(String[] args) {
+
+		int a = 200;
+		
+		System.out.println("10진수 : " + a);
+		System.out.format("8진수 : %o\n", a);
+		System.out.format("16진수 : %x\n", a);
+
+	}
+
+}
