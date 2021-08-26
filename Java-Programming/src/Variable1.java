7d668635f6010d8a3c3d3f9e0afd30a66cd05f83
Kminseung <soccer6504@gmail.com>
2021-08-26 15:46:53 +0900

    Variable - int, float, double, String

---
+++ Java-Programming/src/Variable1.java
@@ -0,0 +1,17 @@
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		
+		int i = 100;
+		float f = 150.5f;
+		double d = 150.5;
+		String s = "강민승";
+		
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(s);
+	}
+
+}
