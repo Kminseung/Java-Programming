39e6d67fe46c6dcec2e3e9ffc814cf15fce91c67
Kminseung <soccer6504@gmail.com>
2021-08-26 16:32:02 +0900

    Conditional - if ~ else

---
+++ Java-Programming/src/LoopConditional/Conditional1.java
@@ -0,0 +1,20 @@
+package LoopConditional;
+
+public class Conditional1 {
+
+	public static void main(String[] args) {
+
+		// if ~ else
+		String s = "I Love You";
+		
+		if(s.contains("Love")) {
+			// Love를 포함하는 경우
+			System.out.println("Me too!");
+		} else {
+			// Love를 포함하지 않는 경우
+			System.out.println("I hate you!");
+		}
+		
+	}
+
+}
