3bd80afac42a70a0cb456140c6d2042914bcc45e
Kminseung <soccer6504@gmail.com>
2021-08-26 16:03:43 +0900

    Data Type - double

---
+++ Java-Programming/src/DataType/Double.java
@@ -0,0 +1,15 @@
+package DataType;
+
+public class Double {
+
+	public static void main(SubString[] args) {
+
+		double a = 10.3;
+		double b = 9.6;
+		double c = 10.1;
+		
+		System.out.println((a + b + c) / 3);
+
+	}
+
+}
