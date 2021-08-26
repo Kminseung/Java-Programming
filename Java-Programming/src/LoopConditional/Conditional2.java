534b1a1f47008b700d9c7988fd6b0db019f8e255
Kminseung <soccer6504@gmail.com>
2021-08-26 16:32:29 +0900

    Conditional - if ~ else if

---
+++ Java-Programming/src/LoopConditional/Conditional2.java
@@ -0,0 +1,22 @@
+package LoopConditional;
+
+public class Conditional2 {
+
+	public static void main(String[] args) {
+
+		// if ~ else if ~ else
+		int score = 95;
+
+		if (score >= 90) {
+			System.out.println("A+입니다.");
+		} else if (score >= 80) {
+			System.out.println("B+입니다.");
+		} else if (score >= 70) {
+			System.out.println("C+입니다.");
+		} else {
+			System.out.println("F입니다.");
+		}
+
+	}
+
+}
