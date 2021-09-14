97233f98774992f2856c92a326005600845acbdc
Kminseung <soccer6504@gmail.com>
2021-09-14 14:08:18 +0900

    Polymorphism - Fruit

---
+++ Java-Programming/src/Polymorphism/Peach.java
@@ -0,0 +1,11 @@
+package Polymorphism;
+
+public class Peach extends Fruit {
+	
+	public Peach() {
+		price = 1500;
+		name = "복숭아";
+		fresh = 75;
+	}
+	
+}
---
+++ Java-Programming/src/Polymorphism/PolyMain1.java
@@ -0,0 +1,15 @@
+package Polymorphism;
+
+public class PolyMain1 {
+
+	public static void main(String[] args) {
+		
+		Fruit peach = new Peach();
+		Fruit banana = new Banana();
+		
+		peach.show();
+		banana.show();
+
+	}
+
+}
